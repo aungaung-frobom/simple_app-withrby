eebb0845b43c3f963a843e52e7caf91a9f4e9d17
aung aung <arkarwunna890.zz@gmail.com>
2017-05-16 13:12:39 +0630

    Initial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	#return th full title #Doument
+	def  full_title (page_title = '') #Methd def optiona;
+		base_title = "Ruby On Rails Tutorials Sample App" #var 
+
+		if page_title.empty ? # boolean test
+			base_title			# return 
+		else
+			page_title + " | " + base_title # String concat
+		end
+
+			
+		end
+		
+	end
 end
