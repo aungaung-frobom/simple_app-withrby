86ec8293facab3e1eac45edacb02e5cda5bfcddb
aung aung <arkarwunna890.zz@gmail.com>
2017-05-23 17:37:57 +0630

    Add following user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,19 @@ def destroy
     redirect_to users_url
   end
 
+def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   def create
     @user = User.new(user_params)
     if @user.save
